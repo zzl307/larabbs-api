08d330da42205e4d0af41c0709043980f016e0bd
zb <zb@exands.com>
2018-05-10 15:20:23 +0800

    web

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -15,8 +15,10 @@ class ReplyObserver
         $topic = $reply->topic;
         $reply->topic->increment('reply_count', 1);
 
-        // 通知作者话题回复
-        $topic->user->notify(new TopicReplied($reply));
+        // 如果评论的作者不是话题的作者，才需要通知
+        if ( ! $reply->user->isAuthorOf($topic)) {
+            $topic->user->notify(new TopicReplied($reply));
+        }
     }
 
     public function creating(Reply $reply)
