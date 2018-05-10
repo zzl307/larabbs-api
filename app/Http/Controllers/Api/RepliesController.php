137c307146af6c1ea8132758146d3c7945924001
zb <zb@exands.com>
2018-05-10 14:42:54 +0800

    web

--- app/Http/Controllers/Api/RepliesController.php
+++ app/Http/Controllers/Api/RepliesController.php
@@ -5,10 +5,25 @@ namespace App\Http\Controllers\Api;
 use App\Http\Requests\Api\ReplyRequest;
 use App\Models\Reply;
 use App\Models\Topic;
+use App\Models\User;
 use App\Transformers\ReplyTransformer;
 
 class RepliesController extends Controller
 {
+	public function index(Topic $topic)
+	{
+		$replies = $topic->replies()->paginate(20);
+
+		return $this->response->paginator($replies, new ReplyTransformer());
+	}
+
+	public function userIndex(User $user)
+	{
+		$replies = $user->replies()->paginate(20);
+
+		return $this->response->paginator($replies, new ReplyTransformer());
+	}
+
     public function store(ReplyRequest $request, Topic $topic, Reply $reply)
     {
     	$reply->content = $request->content;
