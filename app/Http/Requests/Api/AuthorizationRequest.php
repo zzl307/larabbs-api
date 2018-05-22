da46ca819a82bfe6e5812f8b74b89bbba4910d00
zzl307 <qq1013446945@126.com>
2018-05-22 23:05:48 +0800

    web

--- app/Http/Requests/Api/AuthorizationRequest.php
+++ app/Http/Requests/Api/AuthorizationRequest.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Requests\Api;
 
-use Illuminate\Foundation\Http\FormRequest;
-
 class AuthorizationRequest extends FormRequest
 {
     /**
--- app/Http/Requests/Api/UserRequest.php
+++ app/Http/Requests/Api/UserRequest.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Requests\Api;
 
-use Illuminate\Foundation\Http\FormRequest;
-
 class UserRequest extends FormRequest
 {
     /**
